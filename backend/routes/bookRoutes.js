b28b37fe1c1269188cb2d5866b03d18893b81068
Suvanwita <dsuvanwita@gmail.com>
2025-08-26 23:50:25 +0530

    .

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,12 +1,13 @@
 const express=require('express')
 const {getBooks,getBook,createBook,updateBook,deleteBook}=require("../controllers/bookController")
+const {protect,admin}=require("../middleware/authMiddleware")
 
 const router=express.Router()
 
 router.get("/",getBooks)
 router.get("/:id",getBook)
-router.post("/",createBook)
-router.put("/:id",updateBook)
-router.delete("/:id",deleteBook)
+router.post("/",protect,admin,createBook)
+router.put("/:id",protect,admin,updateBook)
+router.delete("/:id",protect,admin,deleteBook)
 
 module.exports=router
